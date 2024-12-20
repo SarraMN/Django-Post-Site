80b7564ab99cd158a75384b1265aa696ad67a082
sarra.mannai <sarra.mannai@bertschinnovation.com>
2024-12-20 16:46:28 +0100

    add share form

--- mysite/settings.py
+++ mysite/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'posts',           # The name of your database
+        'USER': 'root',            # Your PostgreSQL username
+        'PASSWORD': 'admin',       # Your PostgreSQL password
+        'HOST': 'localhost',       # Host for Docker (use 'db' if accessing by service name within a network)
+        'PORT': '5432',            # Default PostgreSQL port
     }
 }
 
